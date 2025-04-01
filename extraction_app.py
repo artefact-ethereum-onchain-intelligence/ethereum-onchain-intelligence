ede314a84b6929be5b9db8d46e74e10106015610
Yohan Fereres <yohan@aria-crypto.com>
2025-04-01 16:24:51 +0000

    Add protocol name per address

--- extraction_app.py
+++ extraction_app.py
@@ -25,7 +25,7 @@ class FetchJSONData():
     
     def call_api_data(self)->json:
         
-        for address in credentials_dict['addresses']:
+        for idx, address in enumerate(credentials_dict['addresses']):
             
             params = {
                 'module': 'account',
@@ -61,8 +61,8 @@ class FetchJSONData():
             # Pretty print the response
             #print(json.dumps(data, indent=4))
             
-            # Shorten address for filename (e.g., first 6 and last 4 characters)
-            short_addr = f"{address[:6]}...{address[-4:]}"
+            # Short name for filename (e.g., first 6 and last 4 characters)
+            short_addr = f"{credentials_dict['addresses_names'][idx]}"
             filename = f"transactions_{short_addr}.json"
             
             self.generate_json_file(data, filename)     
@@ -81,10 +81,12 @@ if __name__ == "__main__":
     key = os.getenv("ETHERSCAN_API_KEY")
     
     addresses_list = ['0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984', '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D', '0xEf1c6E67703c7BD7107eed8303Fbe6EC2554BF6B']
+    names = ['UNI', 'Uniswap V2 Router', 'Uniswap Universal Router']
     credentials_dict = {
                         'api_key': key, \
                         'addresses': addresses_list, \
-                        'transactions_url' : 'https://api.etherscan.io/api'
+                        'transactions_url' : 'https://api.etherscan.io/api', \
+                        'addresses_names' : names
                         }
         
     data_fetch_object = FetchJSONData(credentials_dict)
